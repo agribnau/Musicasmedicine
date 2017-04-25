6d42e4057144d3952bf030410d1fcd3ac4766bd6
SarahPAms <Sarah.L.Polan@gmail.com>
2017-04-25 14:40:31 +0200

    controllers and crude views. Fixed Admin

--- app/admin/content.rb
+++ app/admin/content.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Content do
-  permit_params :info_label, :info_text, :video, :photo, :active
+  permit_params :info_label, :info_text, :video, :photo, :active, :category_id
 end
