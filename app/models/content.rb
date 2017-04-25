24e9d24fa3a4b517c718d06edc02e2cd169359a3
marcia1983 <marcia.albrecht.lorenzini@gmail.com>
2017-04-25 17:23:11 +0200

    Fix validations and add show page

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,10 +1,6 @@
 class Content < ApplicationRecord
   belongs_to :category
 
-  validates :info_label, presence: true
-  validates :info_text, presence: true
-  validates :video, allow_blank: true
-  validates :photo, allow_blank: true
-  validates :active, presence: true
-  validates :website_link, allow_blank: true
+  validates_presence_of :info_label
+
 end
