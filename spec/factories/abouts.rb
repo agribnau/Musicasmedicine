29965cc8e36df55a3680ed81854dd12c0c36defd
Tenicia <tenicia@gmail.com>
2017-04-26 15:24:01 +0200

    Merge probs

---
+++ spec/factories/abouts.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :about do
+    name { Faker::Name.name }
+    photo "http://www.fillmurray.com/200/300"
+    motto "Mundus vult decipi, ergo decipiatur"
+    bio "Het was in de laatste ronde van het Shenzhen International vooral in de ochtend prijs schieten want geen wind, geen regen en zachte greens. Ik speelde ook goed, -3 na 9 en nog gemiste putts op hole 1van 3 meter, hole 4 van 2,5 meetr  en lipouts voor birdie op 8, 10 en 11. Met een beetje geluk was ik op dat moment al -5 of -6 geweest. Op 14 en 16 mis ik helaas van een meter of drie ook voor birdie.  Maar op hole 17 de par-5 viel er eindelijk een lange putt en die was voor eagle en dan ben je toch -5 voor de dag. Helaas geef ik dat gelijk weer weg op 18 door mijn bal in het water te slaan vanaf de tee. Dan loop je toch met een vervelend gevoel na een ronde 69 en een 29ste plaats de baan af. Alles bij elkaar een toernooi met te veel grote fouten. Maar er zijn ook een hoop positieve dingen te melden. Mijn spel naar de vlaggen was weer een stuk beter, dat zie je ook aan het aantal birdies. Dus met veel birdies op naar Beijing voor het Volvo China Open van komende week!"
+  end
+end
