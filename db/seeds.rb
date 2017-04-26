845f382e3cc413129f2aba930fcf29fc9aed0f2f
marcia1983 <marcia.albrecht.lorenzini@gmail.com>
2017-04-26 13:14:22 +0200

    Add testimonial to admin panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 Content.destroy_all
 Category.destroy_all
 
-cat1 = Category.create!(name: 'Music on brain and body', description:'General effect of music on brain and body', photo:'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR3qfIPts6TwYRO0FX40gYKkcpTCZ06FHwDuKqpzTAAz4Ijornvdw', active: true)
-cat2 = Category.create!(name: 'Mmsic in (children\'s) hospital and other care', description: 'General effect of music on children\'s hospital and other care', photo:'https://media.defense.gov/2016/Jan/11/2001333953/670/394/0/160106-F-CQ929-015.JPG', active: true)
+cat1 = Category.create!(name: 'Music on brain and body', description:'General effect of music on brain and body', photo:'http://art2.sewanee.edu/class/fypsummers/files/2014/08/con-doctor.jpg', active: true)
+cat2 = Category.create!(name: 'Music in (children\'s) hospital and other care', description: 'General effect of music on children\'s hospital and other care', photo:'https://sobernation.com/wp-content/uploads/2013/08/music-as-medicine.jpg', active: true)
 cat3 = Category.create!(name: 'Alzheimer / Dementia / Ageing brain', description:'General effect of music on Alzheimer / Dementia / ageing brain', photo:'http://maxpixel.freegreatpicture.com/static/photo/1x/Donald-Trump-Trump-Alzheimers-Warning-Dementia-1750965.jpg', active: true)
-cat4 = Category.create!(name: 'Postoperative pain and anxiety', description:'General effect of music on postoperative pain and anxiety:', photo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL3UhVBDP-TTcxMiXjWQIkrKYk9Ps10r-B2jcQOtNrapVhOwH6mQ', active: true)
-cat5 = Category.create!(name: 'Oncology', description:'General effect of music in oncology', photo:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcScv9U84Yv3On94lHdF_pBRCkyjp_-GC9fSiIVv81ZlLM14J_OM', active: true)
-cat6 = Category.create!(name: 'Depression and anxiety', description:'General effect of music on Depression and anxiety', photo:'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRFieKh1jZcR-5IUTql66N7rBSkMXfbpt6iL9CwGd3cZhDPOZ8K', active: true)
-cat7 = Category.create!(name: 'Parkinson\'s', description:'General effect of music on Parkinson\'s', photo:'http://photos1.blogger.com/hello/54/8285/1024/100_0227.jpg', active: true)
-cat8 = Category.create!(name: 'Coronary Heart Disease', description:'General effect of music on Coronary Heart Disease', photo:'https://c2.staticflickr.com/4/3415/3657889982_6143a52cc9_z.jpg?zz=1', active: true)
-cat9 = Category.create!(name: 'ADHD / Autism', description:'General effect of music on ADHD / Autism', photo:'https://upload.wikimedia.org/wikipedia/commons/1/17/Yayoi_Kusama_Infinity_Room.jpg', active: true)
+cat4 = Category.create!(name: 'Postoperative pain and anxiety', description:'General effect of music on postoperative pain and anxiety:', photo:'http://www.bite-the-dust.com/images/nprmusicmedicine.jpg', active: true)
+cat5 = Category.create!(name: 'Oncology', description:'General effect of music in oncology', photo:'http://www.patientcareonline.com/sites/default/files/styles/max_width/public/images/media/Slide2_260.JPG?itok=BrehM8cE', active: true)
+cat6 = Category.create!(name: 'Depression and anxiety', description:'General effect of music on Depression and anxiety', photo:'http://static.rappler.com/images/science-solitaire-music-as-medicine-20130404.jpg', active: true) 
+cat7 = Category.create!(name: 'Parkinson\'s', description:'General effect of music on Parkinson\'s', photo:'http://images.sxsw.com/XqKZzR7Lbz1qAd_aJ2SxgfFVT9E=/39x30:474x341/950x/images.sxsw.com/27/9aabe10c-c958-4e6a-be8e-7547d61f9374/PP65723.png', active: true)
+cat8 = Category.create!(name: 'Coronary Heart Disease', description:'General effect of music on Coronary Heart Disease', photo:'https://ac-cdn.azureedge.net/infusionnewssiteimages/agingcare/d574b6d7-7cb0-4dda-9e71-5ce089eb4f56.jpg', active: true)
+cat9 = Category.create!(name: 'ADHD / Autism', description:'General effect of music on ADHD / Autism', photo:'http://media.arkansasonline.com/img/photos/2015/06/16/web_music_therapy.jpg', active: true)
 
 cont1=Content.create!(info_label: 'WHAT HAPPENS IN THE BRAIN WHEN YOU LISTEN TO MUSIC?', info_text:'Dutch neuropsychologist prof.dr. Erik Scherder.
 Increase of communication between right and left brain lobe, stimulating COORDINATION between EMOTION AND BEHAVIOR.
