a137cefb0200ad597d3a0bf9ca290cd4f21eee1b
Tenicia <tenicia@gmail.com>
2017-04-26 15:41:33 +0200

    Model test

--- spec/models/about_spec.rb
+++ spec/models/about_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe About, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Active false" do
+    let(:active) { create(:about) }
+    let(:inactive) { create(:about, active: false) }
+
+    it "array without inactive abouts" do
+      About.all.should eq [active]
+    end
+  end
 end
