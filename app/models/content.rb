9379e29a5ebbb7be7d095c0e42ae87d59e70a037
Cesurhan <ces-ur@hotmail.com>
2017-04-26 10:21:56 +0200

    feat/Add backend to Searchbar

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,5 +2,8 @@ class Content < ApplicationRecord
   belongs_to :category
 
   validates_presence_of :info_label
-
+  
+  def self.search(search)
+    where("info_label ILIKE ? OR info_text ILIKE ?", "%#{search}%", "%#{search}%")
+  end
 end
